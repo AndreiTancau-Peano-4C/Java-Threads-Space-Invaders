fe160b5ce40c8b15ebbc0c3f6a06be29bc55dad0
AndreiTancau-Peano-4C <56158376+AndreiTancau-Peano-4C@users.noreply.github.com>
2020-04-02 15:38:02 +0200

    SpaceInvaders step 1

---
+++ SpaceInvaders Step 1/SpaceInvaders.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spaceinvaders;
+
+import javafx.scene.paint.Color;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+/**
+ *
+ * @author Andrei
+ */
+public class SpaceInvaders extends JFrame{
+    
+    public SpaceInvaders(){
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        Intro intro = new Intro();
+        this.add(intro);
+        this.pack();
+        this.setVisible(true);
+        intro.run();
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new SpaceInvaders();
+    }
+
+    
+    
+}
